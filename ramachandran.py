bf19a0ef7000a497469f25d040b980d8ac534576
Lauren Marie Finn <lauren.m.finn@gmail.com>
2021-01-14 19:46:55 +0100

    Include histogram for the chi_1 dihedral angles

--- ramachandran.py
+++ ramachandran.py
@@ -6,19 +6,21 @@ import mdtraj as md
 import os
 import matplotlib.pyplot as plt
 
-# Dictionary for phi and psi indicies
-mims = {"FY1" : ((5,7,9,26),(7,9,26,33)), "FY2" : ((5,7,9,26),(7,9,26,33)), \
-        "FY3" : ((5,7,9,26),(7,9,26,35)), "FY5" : ((5,7,9,27),(7,9,27,41)), \
-        "FY6" : ((5,7,9,26),(7,9,26,32))}
+# Dictionary for phi, psi and chi_1 indicies
+mims = {"FY1" : ((5,7,9,26),(7,9,26,33),(7,9,11,14)), "FY2" : ((5,7,9,26), \
+        (7,9,26,33),(7,9,11,14)), "FY3" : ((5,7,9,26),(7,9,26,35), \
+        (7,9,11,14)), "FY5" : ((5,7,9,27),(7,9,27,41),(7,9,11,14)), \
+        "FY6" : ((5,7,9,26),(7,9,26,32),(7,9,11,14))}
 
+# Set up variables
 home = os.getcwd() 
 font = {'color': 'black', 'weight': 'semibold', 'size': 20}
-
 conformations = []
 for mim in mims.keys():
     conformations.append(mim + "a")
     conformations.append(mim + "b")
 
+# Dihedral angles for both conformations of each mimetic
 for mim in conformations:
     
     # Mimetic name, e.g. "FY1"
@@ -31,25 +33,43 @@ for mim in conformations:
     # Obtain the dihedrals from the trajectory
     traj = md.load("fTyr_md.xtc", top="fTyr_md.gro").remove_solvent()
     atoms, bonds = traj.topology.to_dataframe()
-    phi_indicies, psi_indicies = mims[base]
-    angles = md.compute_dihedrals(traj, [phi_indicies, psi_indicies])
+    phi_indicies, psi_indicies, chi_indicies = mims[base]
+    angles = md.compute_dihedrals(traj, [phi_indicies, psi_indicies, \
+                                         chi_indicies])
     
-    # Create a plot
+    # Create a Ramachandran plot
     fig, ax = plt.subplots(figsize=(15,10))
     plt.scatter(angles[:, 0], angles[:, 1], marker='x', c=traj.time/1000)
     plt.xlabel(r'$\Phi$ Angle [radians]', fontdict=font, labelpad=10)
     cbar = plt.colorbar()
-    cbar.set_label('Time [ns]')
+    cbar.set_label('Time [ns]', fontdict=font, labelpad=10)
+    cbar.ax.set_yticklabels(np.arange(0,100,20),fontsize=16)
+    cbar.ax.tick_params(labelsize=16, direction='out', width=2, length=5)
+    cbar.outline.set_linewidth(3)
     plt.xlim(-np.pi, np.pi)
     plt.ylabel(r'$\Psi$ Angle [radians]', fontdict=font, labelpad=10)
     plt.ylim(-np.pi, np.pi)
-    plt.tick_params(axis='y', labelsize=16, direction='in', width=2, \
-                    length=5)
-    plt.tick_params(axis='x', labelsize=16, direction='in', width=2, \
-                    length=5)
+    plt.tick_params(axis='y', labelsize=20, direction='in', width=2, \
+                    length=5, pad=10)
+    plt.tick_params(axis='x', labelsize=20, direction='in', width=2, \
+                    length=5, pad=10)
     for i in ["top","bottom","left","right"]:
         ax.spines[i].set_linewidth(3) 
     plt.grid(True)
-
     plt.savefig("ramachandranplot.png")
+    plt.show()
+    
+    # Create Chi_1 plot
+    fig, ax = plt.subplots(figsize=(15,10))
+    plt.hist(angles[:, 2], bins=75, color="darkgoldenrod")
+    plt.xlabel(r'$\chi_1$ Angle [radians]', fontdict=font, labelpad=10)
+    plt.xlim(-np.pi, np.pi)
+    plt.tick_params(axis='y', labelsize=20, direction='in', width=2, \
+                    length=5, pad=10)
+    plt.tick_params(axis='x', labelsize=20, direction='in', width=2, \
+                    length=5, pad=10)
+    for i in ["top","bottom","left","right"]:
+        ax.spines[i].set_linewidth(3) 
+    plt.grid(True)
+    plt.savefig("chi1_plot.png")
     plt.show()
